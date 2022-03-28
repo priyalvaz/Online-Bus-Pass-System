40fcf97fb87639dddd29e664720b3c53a1bfaf4e
priyalvaz <67680074+priyalvaz@users.noreply.github.com>
2022-03-28 10:03:40 +0530

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php  
-     	$servername = "umabrisfx8afs3ja.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";  
-       	$username = "kesgfz2lwz2vdgo2";  
-       	$password = "bb6obxfjmg0u8yaj";  
-       	$dbname = "e8xfnsm9jnj53maa";
-       	$conn = mysqli_connect ($servername , $username , $password) or die("unable to connect to host");  
-	$sql = mysqli_select_db ($conn,$dbname) or die("unable to connect to database"); 
-?>
